1967ecc63e165ddcaba2adfc69a7c97fa7138bad
Jorge Gomes <jorge.gomes@vost.pt>
2022-02-05 11:29:48 +0000

    Changes in candidate name

--- legislativas_2022_media_monitor_29jan2022/apps/filter.py
+++ legislativas_2022_media_monitor_29jan2022/apps/filter.py
@@ -9,7 +9,7 @@ import pandas as pd
 import os
 
 # csv to pandas dataframe
-df = pd.read_csv('../legislativas_2022_media_monitor_29jan2022_raw_data/crowdtangle_2022_01_29_inesreal_last_30_days_dataset_raw.csv')
+df = pd.read_csv('../legislativas_2022_media_monitor_29jan2022_raw_data/crowdtangle_2022_01_29_ruitavares_last_30_days_dataset_raw.csv')
 
 # Create Dataframe based on media outlets criteria
 
@@ -44,7 +44,7 @@ df_filter = df[df['Page Name'].isin(['Jornal de Notícias',
 
 # Save filtered dataframe to CSV 
 
-df_filter.to_csv('../legislativas_2022_media_monitor_29jan2022_filter_data/crowdtangle_2022_01_29_inesreal_last_30_days_dataset_filter.csv')
+df_filter.to_csv('../legislativas_2022_media_monitor_29jan2022_filter_data/crowdtangle_2022_01_29_ruitavares_last_30_days_dataset_filter.csv')
 
 
 
@@ -64,14 +64,14 @@ df_c = df_c.rename(columns={'index':'Page Name','Page Name':'Counts'})
 # Plot Candidates main graph 
 
 fig_main = px.bar(df_c, x='Page Name' , y='Counts',template='plotly_white',
-                  title='Inês Sousa Real - Total Mentions by Media Outlet<br><sup>Period:30/12/2021 to 29/01/2022</sup>',
-                  color_discrete_sequence=["#00798E"], 
+                  title='Rui Tavares - Total Mentions by Media Outlet<br><sup>Period:30/12/2021 to 29/01/2022</sup>',
+                  color_discrete_sequence=["#00CE8C"], 
                   text='Counts'
                  )
 
 fig_main.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
 
-fig_main.write_image("../legislativas_2022_media_monitor_29jan2022_graphs/legislativas_2022_images_inesreal_total.png",  width=1920, height=1080, scale=4)
+fig_main.write_image("../legislativas_2022_media_monitor_29jan2022_graphs/legislativas_2022_images_ruitavares_total.png",  width=1920, height=1080, scale=4)
 # fig_main.show()
 
 
