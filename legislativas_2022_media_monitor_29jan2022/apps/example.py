950962d8d301ba5d5f6ca494749cf12663b5b4cd
Jorge Gomes <jorge.gomes@vost.pt>
2022-02-08 15:57:25 +0000

    Example is the Dashboard

---
+++ legislativas_2022_media_monitor_29jan2022/apps/example.py
@@ -0,0 +1,305 @@
+# -*- coding: utf-8 -*-
+
+# Tabs Example 
+
+import time
+
+import dash
+import dash_bootstrap_components as dbc
+import numpy as np
+import plotly.express as px
+import pandas as pd
+from dash import Input, Output, dcc, html
+
+
+# Data treatment 
+
+df = pd.read_csv('https://raw.githubusercontent.com/JorgeMiguelGomes/LEG2022_MediaMonitor/main/legislativas_2022_media_monitor_29jan2022/data_products/legislativas_2022_final_dataset_percentages.csv')
+
+df_individuals  = pd.read_csv('https://raw.githubusercontent.com/JorgeMiguelGomes/LEG2022_MediaMonitor/main/legislativas_2022_media_monitor_29jan2022/data_products/legislativas_2022_all_candidates_filtered.csv')
+
+df_individuals = df_individuals.drop(columns=["Unnamed: 0"])
+df_indivuduals = df_individuals.drop(columns=["Post Created Date"])
+df_individuals_melt=pd.melt(df_individuals,id_vars=['candidato','Page Name'])
+
+
+
+ # -------------- Styling -------------------------------------------
+
+
+candidate_color_map = {
+    "Rui Rio": "#FF6400",
+    "Francisco Rodrigues dos Santos": "#0B6AB1",
+    "António Costa": "#F1A0A2",
+    "Rui Tavares": "#00CE8C",
+    "Catarina Martins": "#C90635",
+    "Jerónimo de Sousa": "#025298",
+    "André Ventura": "#202056",
+    "João Cotrim Figueiredo": "#00AEEF",
+    "Inês Sousa-Real": "#00798E"
+}
+
+pie_color_map = {
+    "Angry":"#666666",
+    "Love":"#5F468F"
+}
+
+
+# Start app 
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = dbc.Container(
+    [
+        # First Row 
+        dbc.Row(
+            [
+            dbc.Col(html.Hr(style={'borderWidth': "2vh", "width": "100%", "borderColor": "#CAE7B9","opacity": "unset"}),width={'size':2}),
+            dbc.Col(html.Hr(style={'borderWidth': "2vh", "width": "100%", "borderColor": "#F3DE8A","opacity": "unset"}),width={'size':2}),
+            dbc.Col(html.Hr(style={'borderWidth': "2vh", "width": "100%", "borderColor": "#EB9486","opacity": "unset"}),width={'size':2}),
+            dbc.Col(html.Hr(style={'borderWidth': "2vh", "width": "100%", "borderColor": "#7E7F9A","opacity": "unset"}),width={'size':2}),
+            dbc.Col(html.Hr(style={'borderWidth': "2vh", "width": "100%", "borderColor": "#97A7B3","opacity": "unset"}),width={'size':2}),
+
+            
+
+            ],className="g-0",
+        ), # end of first row 
+
+
+        # Second Row 
+        dbc.Row( 
+            [ # you have to create a children's array to have more than one column in a row 
+                dbc.Col(html.H3("Legislativas 2022 Media Monitor"), width={'size': 6, 'offset':0}), #First Column
+                dbc.Col(html.H4("Data from CrowdTangle",style={"color":"#7E7F9A"}), width={'size': 5, 'offset':0}), #Second Column
+            ], # Close Children of first Row
+        ), # end of second row  
+
+        # Third Row 
+        dbc.Row(
+            [
+            dbc.Col(html.H5("Observing Media Trends"), width={'size':3, 'offset':0}), # second row
+            dbc.Col(html.H5("Made with Plotly Dash",style={"color":"#97A7B3"}), width={'size':5,'offset':3}),
+            ],
+        ), # end of third row 
+
+        # Fourth Row 
+        dbc.Row(dbc.Col(html.Hr(style={'borderWidth': "0.5vh", "width": "100%", "borderColor": "#F3DE8A","opacity": "unset"}),width={'size':10, 'offset':0}),), 
+
+        # Fith Row - Tabs
+
+        dbc.Tabs(
+            [
+                dbc.Tab(label="About", tab_id="method", label_style={"color": "#CAE7B9"},tab_style={'background-color': '#97A7B3'},active_label_style={"background-color":"#080808"}),      
+                dbc.Tab(label="Metrics by Totals", tab_id="totals", label_style={"color": "#F3DE8A"},tab_style={'background-color': '#7E7F9A'}, active_label_style={"background-color":"#080808"}),
+                dbc.Tab(label="Stacked Analysis", tab_id="stacked", label_style={"color": "#EB9486"},tab_style={'background-color': '#F3DE8A'}, active_label_style={"background-color":"#080808"}),
+                dbc.Tab(label="Love vs Angry", tab_id="love_angry", label_style={"color": "#7E7F9A"},tab_style={'background-color': '#EB9486'}, active_label_style={"background-color":"#080808"}),
+                dbc.Tab(label="Conclusions", tab_id="conclusions", label_style={"color": "#97A7B3"},tab_style={'background-color': '#F3DE8A'}, active_label_style={"background-color":"#080808"}),
+            ],
+            id="tabs",
+            active_tab="method", # this is the tab that will be active when the user comes to the website 
+        ), # end of tabs 
+        html.Div(id="tab-content", className="p-5"),
+    ]
+)
+
+
+
+
+# Callbacks 
+
+# Totals CallBack and Graph 
+
+@app.callback(
+    Output(component_id='graph_totals', component_property='figure'),
+    [Input(component_id='dropdown_total', component_property='value')]
+)
+
+
+def build_graph_total(column_chosen):
+    dff =  df 
+    fig = px.bar(dff,x='Page Name',y=column_chosen,color='candidato',color_discrete_map=candidate_color_map, 
+                template='plotly_white')
+    return fig 
+
+# Stacked Call Back and Graph 
+
+@app.callback(
+    Output(component_id='graph_totals_stacked', component_property='figure'),
+    [Input(component_id='dropdown_total_stacked', component_property='value')]
+)
+
+
+def build_graph_total_stacked(column_chosen):
+    dff =  df 
+    fig_stacked = px.bar(dff,x='Page Name',y=column_chosen,color='candidato',color_discrete_map=candidate_color_map, 
+                template='plotly_white')
+    return fig_stacked
+
+@app.callback(
+    Output(component_id='graph_individuals', component_property='figure'),
+    [Input(component_id='dropdown_candidates', component_property='value')],
+)
+
+
+def build_graph_individuals(column_chosen):
+    dff = df
+    totals_sentiment = dff.groupby(['candidato'])[['Love','Angry']].sum().reset_index()
+    totals_sentiment_melt = pd.melt(totals_sentiment,id_vars="candidato")
+    totals_sentiment_melt = totals_sentiment_melt[totals_sentiment_melt['candidato'] == column_chosen]
+
+
+    fig_individuals = px.pie(totals_sentiment_melt,names="variable",values="value",hole=0.5, color="variable",color_discrete_map=pie_color_map)
+
+    fig_shares_comments = px.pie(totals_sentiment_melt,names="variable",values="value",hole=0.5, color="variable",color_discrete_map=pie_color_map)
+
+    return fig_individuals
+
+@app.callback(
+    Output(component_id='graph_shares_comments', component_property='figure'),
+    
+    [Input(component_id='dropdown_media_outlet', component_property='value')],
+)
+
+def build_graph_shares_comments(column_chosen):
+    dff = df
+    total_shares_comments = dff.groupby(['Page Name'])[['Love','Angry']].sum().reset_index()
+    total_shares_comments_melt = pd.melt(total_shares_comments,id_vars="Page Name")
+    total_shares_comments_melt = total_shares_comments_melt[total_shares_comments_melt['Page Name'] == column_chosen] 
+
+    fig_shares_comments = px.bar(total_shares_comments_melt,x="variable",y="value", color="variable",color_discrete_map=pie_color_map, template='plotly_white')   
+
+    return fig_shares_comments
+
+
+
+@app.callback(Output("tab-content", "children"),
+    [Input("tabs", "active_tab")])
+
+def switch_tab(at):
+    if at == "method":
+        tab1_content = dbc.Row(
+            [
+            dbc.Col(
+                dcc.Markdown('''
+
+### INTRODUCTION
+
+This dashboard shows a quantitative analysis of the mentions that each main candidate, to the Portuguese Legisilative elections had on Facebook, by a set of chosen media outlets.
+
+**Only the title of the Facebook post was filtered**
+
+
+
+### DATASETS 
+
+**Raw Datasets can be found [here](https://github.com/JorgeMiguelGomes/LEG2022_MediaMonitor/tree/main/legislativas_2022_media_monitor_29jan2022/legislativas_2022_media_monitor_29jan2022_raw_data)**
+
+**Full raw dataset can be found [here](https://github.com/JorgeMiguelGomes/LEG2022_MediaMonitor/tree/main/legislativas_2022_media_monitor_29jan2022/data_products)**
+
+These datasets can be used under a [**Creatve Commons License 4.0 BY-SA-NC**](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+
+When using **Crowdtangle** datasets please observe the mandatory references you should make in your work, [**that can be found here**](https://help.crowdtangle.com/en/articles/4558716-understanding-and-citing-crowdtangle-data)
+
+### OBJECTIVE 
+
+Monitor, quantitatively, how many times media outlets in Portugal mention each candidate, in their titles posted to social media, during the pre-electoral and electoral campaign for the 2022 elections
+
+### METHODOLOGY
+
+1.  Search Crowdtangle for every candidate’s respective keywords
+2.  Export to .csv
+3.  Import and treat .csv using Python Scripts 
+4.  Generate Graphics
+5.  Analyze Data
+
+
+
+
+
+
+
+                    '''),
+
+
+                ),
+
+
+            ],
+        )
+
+        return tab1_content 
+
+    elif at == "stacked":
+        tab3_content = dbc.Row(
+                        [
+                            dbc.Col(
+                                dcc.Dropdown(
+                                    id='dropdown_total_stacked',
+                                    options=[
+                                        {'label': 'Total Interactions', 'value': 'Total Interactions_Percentages'} ,
+                                         {'label': 'Total Likes','value': 'Likes_Percentages'},
+                                         {'label': 'Total Comments','value': 'Comments_Percentages'},
+                                         {'label': 'Total Shares','value': 'Shares_Percentages'},
+                                         {'label': 'Total Love','value': 'Love_Percentages'},
+                                         {'label': 'Total Angry','value': 'Angry_Percentages'},
+                                ],
+                                optionHeight=35,                        #height/space between dropdown options
+                                value='Total Interactions_Percentages',             #dropdown value selected automatically when page loads
+                                disabled=False,                         #disable dropdown value selection
+                                multi=False,                            #allow multiple dropdown values to be selected
+                                searchable=True,                        #allow user-searching of dropdown values
+                                search_value='',                        #remembers the value searched in dropdown
+                                placeholder='Please select...',         #gray, default text shown when no option is selected
+                                clearable=True,                         #allow user to removes the selected value
+                                style={'width':"100%"},                 #use dictionary to define CSS styles of your dropdown          
+                                ),            
+                            ),
+                        dbc.Row(dbc.Col(dcc.Graph(id='graph_totals_stacked')),),    
+                        ],
+        ),
+
+        return tab3_content
+
+    elif at == "totals":
+        
+
+        tab2_content = dbc.Row(
+                        [
+                        dbc.Col(
+                            dcc.Dropdown(
+                                id='dropdown_total',
+                                options=[{'label': 'Total Interactions', 'value': 'Total Interactions'} ,
+                                         {'label': 'Total Likes','value': 'Likes'},
+                                         {'label': 'Total Comments','value': 'Comments'},
+                                         {'label': 'Total Shares','value': 'Shares'},
+                                         {'label': 'Total Love','value': 'Love'},
+                                         {'label': 'Total Angry','value': 'Angry'},
+                                    ],
+                                    optionHeight=35,                        #height/space between dropdown options
+                                    value='Total Interactions',             #dropdown value selected automatically when page loads
+                                    disabled=False,                         #disable dropdown value selection
+                                    multi=False,                            #allow multiple dropdown values to be selected
+                                    searchable=True,                        #allow user-searching of dropdown values
+                                    search_value='',                        #remembers the value searched in dropdown
+                                    placeholder='Please select...',         #gray, default text shown when no option is selected
+                                    clearable=True,                         #allow user to removes the selected value
+                                    style={'width':"50"},                 #use dictionary to define CSS styles of your dropdown
+                                    ),
+                            #width={'size':4,'offset':0}
+                            ),
+                        dbc.Row(dbc.Col(dcc.Graph(id='graph_totals')),),
+                        
+                    ],
+                    ),                                             
+
+        return tab2_content
+
+    elif at == "conclusions":
+        tab5_content = html.P("Blah Blab Blaah ")
+        return tab5_content 
+
+    return html.P("This shouldn't ever be displayed...")
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8888)
